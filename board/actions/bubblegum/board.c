f5f3d1c1a34dc9291838cfe7dbf97e5f0cba99b1
pengoor <runangaozhong@163.com>
2016-06-07 22:25:43 -0700

    1. Correct u-boot's definitions, thus u-boot can    run to "board_early_init_f". 2. Add a simple debug interface using four LEDs.

--- board/actions/bubblegum/board.c
+++ board/actions/bubblegum/board.c
@@ -13,8 +13,13 @@
 
 #define GPIOA_OUTEN	(0xe01b0000)
 #define GPIOA_OUTDAT	(0xe01b0008)
+#define DEBUG_LED0_GPIO	(19)	/* GPIOA19 */
+#define DEBUG_LED1_GPIO	(20)	/* GPIOA20 */
 
-#define TEST_LED_GPIO	(19)
+#define GPIOF_OUTEN	(0xe01b00f0)
+#define GPIOF_OUTDAT	(0xe01b00f8)
+#define DEBUG_LED2_GPIO	(1)	/* GPIOF1 */
+#define DEBUG_LED3_GPIO	(2)	/* GPIOF2 */
 
 static struct mm_region bubblegum_mem_map[] = {
 	{
@@ -34,14 +39,38 @@ static struct mm_region bubblegum_mem_map[] = {
 	}
 };
 
+/*
+ * A simple debug function for early debug, in which,
+ * we use four LEDs to display sixteen debug codes, from 0 to 15.
+ * Using it, we can know, at least roughly, at where out code is run.
+ */
+static void bubblegum_early_debug(int debug_code)
+{
+	uint8_t val;
+
+	val = debug_code & 0x1;
+	writel(readl(GPIOA_OUTEN) | (1 << DEBUG_LED0_GPIO), GPIOA_OUTEN);
+	writel(readl(GPIOA_OUTDAT) | (val << DEBUG_LED0_GPIO), GPIOA_OUTDAT);
+
+	val = (debug_code >> 1) & 0x1;
+	writel(readl(GPIOA_OUTEN) | (1 << DEBUG_LED1_GPIO), GPIOA_OUTEN);
+	writel(readl(GPIOA_OUTDAT) | (val << DEBUG_LED1_GPIO), GPIOA_OUTDAT);
+
+	val = (debug_code >> 2) & 0x1;
+	writel(readl(GPIOF_OUTEN) | (1 << DEBUG_LED2_GPIO), GPIOF_OUTEN);
+	writel(readl(GPIOF_OUTDAT) | (val << DEBUG_LED2_GPIO), GPIOF_OUTDAT);
+
+	val = (debug_code >> 3) & 0x1;
+	writel(readl(GPIOF_OUTEN) | (1 << DEBUG_LED3_GPIO), GPIOF_OUTEN);
+	writel(readl(GPIOF_OUTDAT) | (val << DEBUG_LED3_GPIO), GPIOF_OUTDAT);
+}
+
 struct mm_region *mem_map = bubblegum_mem_map;
 
 #ifdef CONFIG_SPL_BUILD
-
 void board_init_f(ulong bootflag)
 {
-	writel(readl(GPIOA_OUTEN) | (1 << TEST_LED_GPIO), GPIOA_OUTEN);
-	writel(readl(GPIOA_OUTDAT) | (1 << TEST_LED_GPIO), GPIOA_OUTDAT);
+	bubblegum_early_debug(1);
 	while (1);
 }
 
@@ -50,19 +79,29 @@ void panic(const char *fmt, ...)
 }
 #endif
 
+int board_early_init_f(void)
+{
+	bubblegum_early_debug(1);
+	while (1);
+
+	return 0;
+}
+
+int board_init(void)
+{
+	return 0;
+}
+
+struct serial_device *default_serial_console(void)
+{
+	return NULL;
+}
+
 void reset_cpu(ulong addr)
 {
 }
 
 int dram_init(void)
 {
-}
-
-int board_init(void)
-{
-}
-
-struct serial_device *default_serial_console(void)
-{
-	return NULL;
+	return 0;
 }
--- include/configs/bubblegum.h
+++ include/configs/bubblegum.h
@@ -10,26 +10,36 @@
 #ifndef __BUBBLEGUM_H
 #define __BUBBLEGUM_H
 
-/* share ram start+header */
+/*
+ * u-boot SPL definitions, which is resided in SRAM
+ */
+
 #define CONFIG_SPL_TEXT_BASE		0xe406b200
 #define CONFIG_SPL_MAX_SIZE		(1024 * 20)
 
+#define CONFIG_SPL_BSS_START_ADDR	(CONFIG_SPL_TEXT_BASE + \
+					 CONFIG_SPL_MAX_SIZE)
+#define CONFIG_SPL_BSS_MAX_SIZE		(1024 * 16)
+
 #define CONFIG_SPL_STACK		0xe407f000
 
-#define CONFIG_SPL_BSS_START_ADDR	0xe4068400
-#define CONFIG_SPL_BSS_MAX_SIZE		0x00004000	/* 16 KB */
 
-/* SDRAM Definitions */
+/*
+ * SDRAM Definitions
+ */
+
 #define CONFIG_SYS_SDRAM_BASE		0x0
 #define CONFIG_NR_DRAM_BANKS		1
 
-/* Link Definitions */
-#define CONFIG_SYS_TEXT_BASE		0x11000000
-#define CONFIG_SYS_INIT_SP_ADDR		(CONFIG_SYS_SDRAM_BASE + 0x7ff00)
+/*
+ * u-boot definitions, which is resided in SDRAM, TODO
+ */
 
-/* Some commands use this as the default load address */
-#define CONFIG_SYS_LOAD_ADDR		(CONFIG_SYS_SDRAM_BASE + 0x7ffc0)
+#define CONFIG_SYS_TEXT_BASE		CONFIG_SPL_TEXT_BASE
+#define CONFIG_SYS_INIT_SP_ADDR		CONFIG_SPL_STACK
 
+/* Some commands use this as the default load address, TODO */
+#define CONFIG_SYS_LOAD_ADDR		(CONFIG_SYS_SDRAM_BASE)
 
 /* Generic Interrupt Controller Definitions */
 #define GICD_BASE			(0xe00f1000)
@@ -40,16 +50,17 @@
 #define CONFIG_ENV_SIZE			0x1000	/* 4K Bytes */
 
 /* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		(32 * 1024 * 1024 + CONFIG_ENV_SIZE)
+#define CONFIG_SYS_MALLOC_LEN		(16 * 1024 * 1024 + CONFIG_ENV_SIZE)
 
 #define CONFIG_SYS_CBSIZE		512	/* Console I/O Buffer Size */
 #define CONFIG_SYS_MAXARGS		16	/* max command args */
-
 
 /* UART Definitions */
 #define CONFIG_BAUDRATE			115200
 
 /* FLASH and environment organization */
 #define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_BOARD_EARLY_INIT_F
 
 #endif
